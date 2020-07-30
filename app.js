4ea06334dd7820a7b56d61b1a44be935458fd898
theague <theague@gmail.com>
2020-07-29 19:43:01 -0700

    Steps 1 and 2 done

---
+++ app.js
@@ -0,0 +1,12 @@
+const donaldDuck = {
+    createdBy: 'Walt Disney',
+    name: 'Donald Duck',
+    nickname: 'Don',
+    species: 'Duck',
+    gender: 'Male',
+    significantOther: 'Daisy Duck'
+
+}
+
+console.log(donaldDuck.name);
+
