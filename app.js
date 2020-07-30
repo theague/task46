ab9a1d1fa84e1a798152f7489d5dd5cae6e8d933
theague <theague@gmail.com>
2020-07-30 01:40:29 -0700

    Final commit for task

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Step 1
+//Step 1 - create an object
 const donaldDuck = {
     createdBy: 'Walt Disney',
     name: 'Donald Duck',
@@ -7,19 +7,49 @@ const donaldDuck = {
     gender: 'Male',
     significantOther: 'Daisy Duck'
 
-}
-//Step 2
+};
+//Step 2 print name to console
 console.log(donaldDuck.name);
 
-//Step 3
+//Step 3 add a new property
 donaldDuck.canFly = false;
 
-//Step 4
+//Step 4 add a new method
 const dack = {
     say() {
-        return "DACK DACK! Is this what a duck sounds like?"
+        return "DACK DACK! Is this what a duck sounds like?";
     }
 };
 
 console.log(dack.say());
 
+//Step 5 add another method
+donaldDuck.sayHello = function () {
+    console.log(this.name);
+};
+
+//Step 6 call new method
+donaldDuck.sayHello();
+
+//Step 7 modify ducks name property
+donaldDuck.name = "Duck McDuckFace";
+
+donaldDuck.sayHello();
+
+//Step 8 add another method with if/else
+donaldDuck.fly = function() {
+    if (this.canFly == true) {
+    console.log("I can fly!");
+    } else {
+    console.log("No flying for me!")
+    }
+};
+
+// Step 9 call the method
+donaldDuck.fly();
+
+//Step 10 modify property
+donaldDuck.canFly = true;
+
+//Step 11 call the method again with new property value
+donaldDuck.fly();
