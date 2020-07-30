4f5cf615463a9e5f011cb340aa2a3f70e5bff820
theague <theague@gmail.com>
2020-07-29 20:37:49 -0700

    Update for step 4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Step 1
 const donaldDuck = {
     createdBy: 'Walt Disney',
     name: 'Donald Duck',
@@ -7,6 +8,18 @@ const donaldDuck = {
     significantOther: 'Daisy Duck'
 
 }
-
+//Step 2
 console.log(donaldDuck.name);
+
+//Step 3
+donaldDuck.canFly = false;
+
+//Step 4
+const dack = {
+    say() {
+        return "DACK DACK! Is this what a duck sounds like?"
+    }
+};
+
+console.log(dack.say());
 
